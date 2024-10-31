85749f075ecf529b8d86fce162a5d67871897555
Rhoydel Jr <154987395+rhoii@users.noreply.github.com>
2024-10-31 13:45:18 +0800

    Add files via upload

---
+++ DataCasting.java
@@ -0,0 +1,9 @@
+public class DataCasting {
+    public static void main (String[] args){
+        int num = 1000;
+        double myDouble = num;
+
+        System.out.println("Int to Double: " +myDouble);
+    }
+    
+}
